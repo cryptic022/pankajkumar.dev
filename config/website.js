04ad2ae7259e295fa2ae36a594ff8d2fe14b0ac2
Pankaj Kumar <pankaj.kumar@autodesk.com>
2019-05-25 15:24:04 +0530

    Updated-title-and-social-handles

--- config/website.js
+++ config/website.js
@@ -1,17 +1,17 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
-  siteTitle: 'My Blog', // Navigation and Site Title
-  siteTitleAlt: 'The blog of an egghead creator', // Alternative Site title for SEO
+  siteTitle: 'Pankaj Kumar Blog', // Navigation and Site Title
+  siteTitleAlt: 'The blog of Pankaj Kumar', // Alternative Site title for SEO
   siteTitleShort: 'Starter Blog', // short_name for manifest
-  siteUrl: 'https://your-site.io', // Domain of your site. No trailing slash!
+  siteUrl: 'https://pankajkumar.dev', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
   siteDescription: 'This is where they post things!',
   author: 'Author', // Author for schemaORGJSONLD
-  organization: 'egghead.io LLC',
+  organization: 'Pankaj Kumar',
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@eggheadio', // Twitter Username
+  userTwitter: '@cryptic022', // Twitter Username
   ogSiteName: 'egghead.io Gatsby Starter Blog', // Facebook Site Name
   ogLanguage: 'en_US',
   googleAnalyticsID: '',
@@ -21,8 +21,8 @@ module.exports = {
   backgroundColor: '#2b2e3c',
 
   // Social component
-  twitter: 'https://twitter.com/eggheadio/',
-  twitterHandle: '@eggheadio',
-  github: 'https://github.com/eggheadio/',
-  linkedin: '',
+  twitter: 'https://twitter.com/cryptic22/',
+  twitterHandle: '@cryptic22',
+  github: 'https://github.com/cryptic22/',
+  linkedin: 'https://www.linkedin.com/in/cryptic22/',
 }
